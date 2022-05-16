49d6208fd5980153a586de41a4af5acc86670846
dabarrio <davidbarriosok25@gmail.com>
2022-05-16 10:44:55 -0300

    Fix

--- src/Components/AppTodo.jsx
+++ src/Components/AppTodo.jsx
@@ -53,7 +53,7 @@ const AppTodo = () => {
                     value={value}
                     className={css.inputSearch}
                 />
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-plus" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round" onClick={handleClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-plus" width="60" height="60" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round" onClick={handleSubmit}>
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <rect x="4" y="4" width="16" height="16" rx="2" />
                     <line x1="9" y1="12" x2="15" y2="12" />
